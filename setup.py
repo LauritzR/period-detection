4851be44f15e9e97a014a1bd96791fd73e78a6d5
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2023-01-24 16:37:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="period-detection",
-    version="1.0.2",
+    version="1.0.3",
     author="Tim Breitenbach & Lauritz Rasbach",
     author_email="tim.breitenbach@mathematik.uni-wuerzburg.de, rasbachlauritz@googlemail.com",
     description="",
@@ -19,5 +19,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=['numpy','scipy','pandas','scikit-learn','matplotlib']
+    install_requires=['numpy','scipy','pandas','scikit-learn','matplotlib', 'collections']
 )
